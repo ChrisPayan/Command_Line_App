dd9d06fc6732b4714e8d4fbbb372e561a09c2dbe
Christian Payan <payanstocks@gmail.com>
2021-04-21 23:13:40 -0700

    Added README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Command_Line_App
+# Command Line App
+
+## Description
+In this program it allows a company to completely build their department, role, and employee database from one location cycling through all the perameters for each section until the user is satisfied. Also allowing the useto be able to search and make changes to the existing data.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+Once the app is cloned make sure to install all dependencies using npm install before running the application
+## Usage
+A video detailing instruction is attached ---> 
+
+    screenshot
+
+## Credits
+
+- [javierperez98](https://github.com/javierperez98)
+
+## Contact
+* GitHub : [ChrisPayan](https://github.com/ChrisPayan)
+* LinkedIn : 
+* E-mail : [payanstocks@gmail.com](payanstocks@gmail.com)
+
+## License
+N/A
--- company.sql
+++ company.sql
@@ -1,24 +1,42 @@
+DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE employee (
-	id integer not null auto_increment,
-    first_name varchar(30) NOT NULL,
-    last_name varchar(30) NOT NULL,
-    role_id integer,
-    manager_id integer,
-    primary key(id)
-);
-CREATE TABLE role (
-	id integer not null auto_increment,
-    title varchar(30),
-    salary decimal(10, 2),
-    department_id integer,
-    primary key(id)
-);
+
 CREATE TABLE department(
 	id integer not null auto_increment,
     name varchar(30),
     primary key(id)
 );
+
+CREATE TABLE role (
+	id integer not null auto_increment,
+    title varchar(30),
+    salary decimal(10, 2),
+    department_id integer,
+    FOREIGN KEY(department_id) REFERENCES department(id),
+    primary key(id)
+);
+CREATE TABLE employee (
+	id integer not null auto_increment,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id integer,
+	FOREIGN KEY(role_id) REFERENCES role(id),
+    manager_id integer,
+    FOREIGN KEY(manager_id) REFERENCES employee(id),
+    primary key(id)
+);
+Select * From role;
+SELECT name, id as value FROM department;
+SELECT role.title , role.salary , name  FROM role LEFT JOIN department ON department_id = department.id;
+
+SELECT CONCAT(employee.first_name,' ', employee.last_name) AS Employee, 
+role.title AS Position, department.name AS Department, role.salary AS Salary, 
+CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+FROM employee
+LEFT JOIN employee manager on manager.id = employee.manager_id
+INNER JOIN role ON (role.id = employee.role_id)
+INNER JOIN department ON (department.id = role.department_id)
+ORDER BY department;
